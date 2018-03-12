800a04cec56b56e45f9dd60200838574f6649f5a
Marco N <sainoba@gmal.com>
2018-03-12 00:01:19 -0600

    Added abc, decorator, generator base files

---
+++ abc_exercise/library.py
@@ -0,0 +1,10 @@
+#library.py
+
+# Make sure classes that inherit from `Base` require to 
+# implement `self.baz`. Hint: Use abc
+# - 
+
+
+class Base:
+    def bar():
+        return "bar"
---
+++ abc_exercise/user.py
@@ -0,0 +1,12 @@
+#user.py
+from .library import Base
+
+# Make sure that if self.bar doesn't exist a NotImplementedError
+# exception is thrown.
+# you can't modify `baz()`
+# you can't modify library.py for this
+
+
+class Derived(Base):
+    def baz():
+        return "baz" + self.bar()
